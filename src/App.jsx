c1fc9835dd7bc22a3fdc44faf43045be49af19ca
hecluus <heccluis@gmail.com>
2025-10-19 14:41:19 -0300

    Se agrega el componete header y ademas se colocan las rutas para usar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'
 import { Footer } from './components/footer'
 import './styles/footer.css'
+import { Header } from './components/Header'
 import { Reservaf5 } from './pages/reservaf5';
 import './styles/reserva.css'
-
 
 function App() {
 
   return (
     <>
-      <Reservaf5/>
-      <Footer/>
+      <Header />
+      <Routes>
+        <Route path='/' Component={Reservaf5} />
+{/*         <Route path='/reservas' Component={Reservaf5} />
+        <Route path='/comida' Component={Reservaf5} />
+        <Route path='/contacto' Component={Reservaf5} />
+        <Route path='/sobrenosotros' Component={Reservaf5} />
+        <Route path='/iniciarsesion' Component={Reservaf5} />
+        <Route path='/registrar' Component={Reservaf5}/> */}
+      </Routes>
+      <Footer />
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,51 @@
+import '../styles/general.css'
+import '../styles/Header.css'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import { Link } from 'react-router-dom'
+
+export const Header = () => {
+  return (
+    <header>
+      <Container fluid className="nav-contenedor">
+        <Nav className="header-nav">
+          <Link to='/' className="nav-links">
+            Inicio
+          </Link>
+          <Link to='' className="nav-links" >
+            Reserva-F5
+          </Link>
+          <Link to='' className="nav-links" >
+            Comida
+          </Link>
+          <Link to='' className="nav-links">
+            Contacto
+          </Link>
+          <Link to='' className="nav-links">
+            Sobre Nosotros
+          </Link>
+          <form className="d-flex form-buscador ms-3" role="search">
+            <input
+              aria-label="Buscar"
+              className="form-control me-2 buscador"
+              placeholder="Buscar"
+              type="search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Buscar
+            </button>
+          </form>
+        </Nav>
+
+        <div className="login-btns">
+          <Link to='' className="btn btn-primary btn-login" >
+            Iniciar Sesión
+          </Link>
+          <Link to='' className="btn btn-primary btn-login">
+            Registrar
+          </Link>
+        </div>
+      </Container>
+    </header>
+  );
+}
